6d51e91496b4af23243e61d700b22b688308fbb5
Victor Cordero <victordcordero@gmail.com>
2020-10-21 22:11:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,15 +1,26 @@
 def hopper
-	programmer_hash =
+
 	programmer_hash =
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-@@ -16,13 +16,13 @@ def hopper
-        }
-     }
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
 
+end
 
-  programmer_hash[:grace_hopper]
+programmer_hash[:grace_hopper]
+
+programmer_hash
 end
 
 def alan_kay_is_known_for
