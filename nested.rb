dbf980727d475c9e86f07b6fd455ec747d95d420
Victor Cordero <victordcordero@gmail.com>
2020-10-21 22:01:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -21,8 +21,6 @@ end
 
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-
 programmer_hash[:alan_kay][0]
 
 	programmer_hash =
